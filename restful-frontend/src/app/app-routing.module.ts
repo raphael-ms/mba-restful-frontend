ab6f39435cc4d89297610c53bbe417315cd697e9
Raphael Marques <raphaelmarques77@gmail.com>
2021-02-28 19:45:27 -0300

    Implement cpf finder

--- restful-frontend/src/app/app-routing.module.ts
+++ restful-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PessoaComponent } from './pessoa/pessoa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PessoaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
